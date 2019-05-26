a22e7fcea602a3ad1f09e85bd97f671ffc4f24f7
guttyar-2213 <yukkurityoya@yahoo.co.jp>
2019-05-26 15:51:11 +0900

    to SPA

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import pkg from './package'
 
 export default {
-  mode: 'universal',
+  mode: 'spa',
 
   /*
    ** Headers of the page
@@ -29,7 +29,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{ src: '@/plugins/element-ui', ssr: false }],
+  plugins: ['@/plugins/element-ui'],
 
   /*
    ** Nuxt.js modules
